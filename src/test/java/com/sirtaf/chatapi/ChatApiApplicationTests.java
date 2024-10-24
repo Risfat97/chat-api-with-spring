2f30feadd7d3d3bab7cf0206188ba1c396570aef
Risfat97 <tamsir97@gmail.com>
2024-10-24 15:21:05 +0200

    Initializing project

---
+++ src/test/java/com/sirtaf/chatapi/ChatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirtaf.chatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
